b73fb9587f3fb3ca515380c754f9234244a48816
Abid Kidwai <abidkidwai786@gmail.com>
2022-10-07 18:36:58 +0000

    First Commit

--- tests/nightwatchTest.js
+++ tests/nightwatchTest.js
@@ -2,11 +2,12 @@ const Device = require('selenium-webdriver/lib/input').Pointer;
 describe('Nightwatch app tests', function() {
    
   it('swipe down and zoom in the page - native gestures api', function() {
-    browser.click('color');
-    // using appium non-standard gestures api.
-    browser.execute('mobile: swipe', {direction: 'up', velocity: 5000}); // swipe up the page
-    browser.execute('mobile: pinch', {scale: '1.5', velocity: 1.1}); // zoom in
+   
+    browser.useXpath().click("(//*[contains(@class,'android.widget.EditText')])[1]")
     browser.pause(4000);
-  });
-    
+
+    browser.useXpath().click("(//*[contains(@class,'android.widget.EditText')])[2]")
+    browser.pause(4000);
+
+  });    
 });
